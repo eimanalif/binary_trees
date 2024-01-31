bbd96e6aba343994873db4d6c8e722b8f5b54412
eimanalif <eimanalif@eiman.fadlelmowla@gmail.com>
2024-02-01 00:19:27 +0400

    2-binary_tree_node.c

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -2,7 +2,8 @@
 
 
 /**
- *binary_tree_insert_right - function that inserts a node as the right-child of another node
+ *binary_tree_insert_right - function that inserts a node as 
+ 				the right-child of another node
  *Description:function that inserts a node as the right-child of another node
  *@perant:is the root node
  *@value:is a data in the node
