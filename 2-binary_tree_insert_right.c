fa9df101acfc1bad757d9264ec77933a4015a234
eimanalif <eimanalif@eiman.fadlelmowla@gmail.com>
2024-02-01 00:23:34 +0400

    2-binary_tree_node.c

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -15,21 +15,21 @@
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new;
+	binary_tree_t *new_node;
 
 	if (parent == NULL)
 		return (NULL);
 
-	new = binary_tree_node(parent, value);
-	if (new == NULL)
+	new_node = binary_tree_node(parent, value);
+	if (new_node == NULL)
 		return (NULL);
 
 	if (parent->right != NULL)
 	{
-		new->right = parent->right;
-		parent->right->parent = new;
+		new_node->right = parent->right;
+		parent->right->parent = new_node;
 	}
-	parent->right = new;
+	parent->right = new_node;
 
-	return (new);
+	return (new_node);
 }
