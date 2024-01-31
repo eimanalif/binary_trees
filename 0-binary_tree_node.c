05bc9dc7d9cf348fe1d0baeb3e3bd2b0e6e65042
eimanalif <eimanalif@eiman.fadlelmowla@gmail.com>
2024-01-31 22:41:42 +0400

    0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -2,11 +2,11 @@
 
 
 /**
- * binary_tree_node - a function that creates a binary tree node
- * Description:a function that creates a binary tree node
- * @perant:is the root node
- * @value:is a data in the node
- * Return:binary tree
+ *binary_tree_node - a function that creates a binary tree node
+ *Description:a function that creates a binary tree node
+ *@perant:is the root node
+ *@value:is a data in the node
+ *Return:binary tree
 */
 
 
@@ -21,7 +21,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	new->n = value;
 	new->parent = parent;
 	new->left = NULL;
- 	new->right = NULL;
+	new->right = NULL;
 
 	return (new);
 }
