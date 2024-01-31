0a0cce39bcae217eb86dd0c6880531c0eacdf56e
eimanalif <eimanalif@eiman.fadlelmowla@gmail.com>
2024-02-01 01:52:45 +0400

    -binary_tree_node.c

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -2,9 +2,9 @@
 
 /**
  * binary_tree_preorder - function that goes through a binary
- * 							tree using pre-order traversal
+ *                          tree using pre-order traversal
  * Description: Where tree is a pointer to the root node of the tree to traverse
- * 				And func is a pointer to a function to call for each node.
+ *              And func is a pointer to a function to call for each node.
  * @tree:a pointer to the root node of the tree to traverse
  * @func:is a pointer to a function to call for each node.
  * Return:void
