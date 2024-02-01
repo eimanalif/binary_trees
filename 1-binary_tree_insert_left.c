08cdd4acf1f33dab476a115a5575c8f0ac94b6e9
eimanalif <eimanalif@eiman.fadlelmowla@gmail.com>
2024-02-02 00:37:34 +0400

    104-binarytree

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,6 +1,5 @@
 #include "binary_trees.h"
 
-
 /**
  * binary_tree_insert_left - Inserts node as a left-child of
  *                           of another in a binary tree.
@@ -14,8 +13,6 @@
  *              takes its place and the old left-child is set as
  *              the left-child of the new node.
  */
-
-
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
