0527d0b5b6682582a0dd87c58bbaa938da2b317f
eimanalif <eimanalif@eiman.fadlelmowla@gmail.com>
2024-02-02 00:35:25 +0400

    104-binarytree

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,9 +1,8 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_node - Creates a binary tree node.
- * Description: Creates a binary tree node.
- * @parent: A pointer to the parent of the node to create.
+ * binary_tree_node - Creates binary tree node.
+ * @parent: A pointer to parent of the node to create.
  * @value: The value to put in the new node.
  *
  * Return: If an error occurs - NULL.
